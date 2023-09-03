935004a6c8aa06fc1750d5292e8f93617a64738f
ivanpravada <vano.provada@gmail.com>
2023-09-03 15:34:30 +0300

    Add year converter to the url patterns

--- site2023/somes/views.py
+++ site2023/somes/views.py
@@ -6,5 +6,13 @@ def index(request):
     return HttpResponse("Somes site")
 
 
-def categories(request):
-    return HttpResponse("<h1>Categories</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Categories by id</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Categories by slug</h1><p>id: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Archive by year</h1><p>{year}</p>")
